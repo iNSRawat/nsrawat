53ed50c1b7b17652cf1f98636db4b45eefe6682a
iNSRawat <iNSRawat@users.noreply.github.com>
2026-01-25 09:07:25 +0530

    fix: update navbar github link to use utm source

--- src/components/nav-item-github.tsx
+++ src/components/nav-item-github.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
-import { SOURCE_CODE_GITHUB_REPO } from "@/config/site";
+import { Button } from "@/components/ui/button";
+import { SOURCE_CODE_GITHUB_URL } from "@/config/site";
 
 export function NavItemGitHub() {
   return (
     <Button variant="ghost" size="icon" asChild>
       <a
-        href={`https://github.com/${SOURCE_CODE_GITHUB_REPO}`}
+        href={SOURCE_CODE_GITHUB_URL}
         target="_blank"
         rel="noopener noreferrer"
         aria-label="GitHub"
