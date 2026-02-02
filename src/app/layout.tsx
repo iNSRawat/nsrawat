be43bfe482daab52e20b27ccbf9fb3134e94ad64
iNSRawat <iNSRawat@users.noreply.github.com>
2026-02-02 11:45:01 +0530

    fix: resolve ChunkLoadError in layout.tsx by using Buffer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,7 +130,11 @@ export default function RootLayout({
           Thanks @tailwindcss. We inject the script via the `<Script/>` tag again,
           since we found the regular `<script>` tag to not execute when rendering a not-found page.
          */}
-        <Script src={`data:text/javascript;base64,${btoa(darkModeScript)}`} />
+        <Script
+          src={`data:text/javascript;base64,${Buffer.from(
+            darkModeScript,
+          ).toString("base64")}`}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
