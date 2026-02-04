c44d6a220634c49ff13366c0c64c128054962a32
iNSRawat <iNSRawat@users.noreply.github.com>
2026-02-04 11:16:39 +0530

    fix: replace CD logo with NSR mark on 404 page

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -9,17 +9,18 @@ export function NotFound({ className }: { className?: string }) {
     <div
       className={cn(
         "flex h-[calc(100svh-5.5rem)] flex-col items-center justify-center",
-        className
+        className,
       )}
     >
       <svg
         className="h-28 w-full text-border"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 514 258"
+        viewBox="0 0 448 160"
         fill="none"
       >
+        {/* NSR Pixel Mark in outline style */}
         <path
-          d="M65 193v64h128v-64H65Zm0 0H1V65h64m0 128V65m384 0H321v128h128m0-128V1H257v256h192v-64m0-128v128m0-128h64v128h-64M65 65h128V1H65v64Z"
+          d="M0 0h32v32h-32zM96 0h32v32h-32zM0 32h32v32h-32zM32 32h32v32h-32zM96 32h32v32h-32zM0 64h32v32h-32zM64 64h32v32h-32zM96 64h32v32h-32zM0 96h32v32h-32zM96 96h32v32h-32zM0 128h32v32h-32zM96 128h32v32h-32zM192 0h32v32h-32zM224 0h32v32h-32zM256 0h32v32h-32zM160 32h32v32h-32zM192 64h32v32h-32zM224 64h32v32h-32zM256 96h32v32h-32zM160 128h32v32h-32zM192 128h32v32h-32zM224 128h32v32h-32zM320 0h32v32h-32zM352 0h32v32h-32zM384 0h32v32h-32zM320 32h32v32h-32zM416 32h32v32h-32zM320 64h32v32h-32zM352 64h32v32h-32zM384 64h32v32h-32zM320 96h32v32h-32zM384 96h32v32h-32zM320 128h32v32h-32zM416 128h32v32h-32z"
           stroke="currentColor"
           strokeWidth="1"
           vectorEffect="non-scaling-stroke"
