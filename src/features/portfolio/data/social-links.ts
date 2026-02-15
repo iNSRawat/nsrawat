2be333fbc7b5bddadc6f1e5f6a60633cfa864758
iNSRawat <iNSRawat@users.noreply.github.com>
2026-02-15 17:23:54 +0530

    Update social icons order and footer links

--- src/features/portfolio/data/social-links.ts
+++ src/features/portfolio/data/social-links.ts
@@ -2,6 +2,12 @@ import type { SocialLink } from "../types/social-links";
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
+    icon: "x",
+    title: "X (formerly Twitter)",
+    description: "@NSRawat_in",
+    href: "https://twitter.com/NSRawat_in?utm_source=nsrawat.in",
+  },
+  {
     icon: "github",
     title: "GitHub",
     description: "iNSRawat",
@@ -14,25 +20,19 @@ export const SOCIAL_LINKS: SocialLink[] = [
     href: "https://www.linkedin.com/in/insrawat?utm_source=nsrawat.in",
   },
   {
-    icon: "kaggle", // Need to ensure knaggle is supported or fallback
-    title: "Kaggle",
-    description: "nsrawat",
-    href: "https://www.kaggle.com/nsrawat?utm_source=nsrawat.in",
-  },
-  {
-    icon: "x",
-    title: "X (formerly Twitter)",
-    description: "@NSRawat_in",
-    href: "https://twitter.com/NSRawat_in?utm_source=nsrawat.in",
-  },
-  {
-    icon: "peerlist", // Need ensure supported
+    icon: "peerlist",
     title: "Peerlist",
     description: "nsrawat",
     href: "https://peerlist.io/nsrawat?utm_source=nsrawat.in",
   },
   {
-    icon: "datacamp", // Need ensure supported
+    icon: "kaggle",
+    title: "Kaggle",
+    description: "nsrawat",
+    href: "https://www.kaggle.com/nsrawat?utm_source=nsrawat.in",
+  },
+  {
+    icon: "datacamp",
     title: "DataCamp",
     description: "ns-rawat",
     href: "https://www.datacamp.com/portfolio/ns-rawat?utm_source=nsrawat.in",
