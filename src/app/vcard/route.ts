c6e46aca3d78e472e77ca548f848658f95212ac2
Nagendra Singh Rawat <iNSRawat@users.noreply.github.com>
2026-09-10 20:36:44 +0530

    chore(deps): update dependencies to latest tech stack

--- src/app/vcard/route.ts
+++ src/app/vcard/route.ts
@@ -11,19 +11,19 @@ export async function GET() {
   const card = new VCard();
 
   card
-    .addName(USER.lastName, USER.firstName)
-    .addAddress(USER.address)
-    .addEmail(decodeEmail(USER.email))
-    .addURL(USER.website);
+    .addName({ familyName: USER.lastName, givenName: USER.firstName })
+    .addAddress({ street: USER.address })
+    .addEmail({ address: decodeEmail(USER.email) })
+    .addUrl({ url: USER.website });
 
   const photo = await getVCardPhoto(USER.avatar);
   if (photo) {
-    card.addPhoto(photo.image, photo.mine);
+    card.addPhoto({ image: photo.image, mime: photo.mine });
   }
 
   if (USER.jobs.length > 0) {
     const company = USER.jobs[0];
-    card.addCompany(company.company).addJobtitle(company.title);
+    card.addCompany({ name: company.company }).addJobtitle(company.title);
   }
 
   return new NextResponse(card.toString(), {
